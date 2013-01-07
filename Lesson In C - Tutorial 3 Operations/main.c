3617145c6835bb4a18ec8404dc3b69b8845e2a0c
Hana Sakamoto <sarcrates_dev@icloud.com>
2013-01-07 14:22:49 -0700

    Initial Commit

---
+++ Lesson In C - Tutorial 3 Operations/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lesson In C - Tutorial 3 Operations
+//
+//  Created by sarcrates on 1/7/13.
+//  Copyright © 2013 sarcrates. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
